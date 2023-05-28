fcc7b80965ab8611ac4a6fd1a359954347d917df
Trickerer01 <onlysuffering+1@gmail.com>
2023-05-28 14:05:55 +0700

    spys: replace evals and exec with actual algorithms

--- src/module/px_grab_spys.py
+++ src/module/px_grab_spys.py
@@ -7,6 +7,7 @@ Author: trickerer (https://github.com/trickerer)
 #
 
 from re import compile as re_compile, search as re_search, sub as re_sub
+from typing import Dict
 
 from requests import Session
 
@@ -44,13 +45,22 @@ def grab_proxies() -> None:
                 content_str = preq.content.decode()
                 preq.close()
                 p, x = tuple(re_search(r'}\((\'[^.]+)', content_str).group(1).split(',60,60,'))
-                p, x = p[1:-1], x[1:-1].split('^')
-                chr_arr = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX'
-                s = {chr_arr[i]: x[i] or chr_arr[i] for i in range(len(chr_arr))}
-                p1 = re_sub(r'\b(\w+)\b', lambda w: s.get(w.group(1)) or '\'\'', p)
-                assert p1.find('(') == -1  # nothing callable here, only assignments, ex Zero=0
-                exec(p1)
-                locs = locals()  # required to pass to eval()
+                p_exec, symbols = p[1:-2], x[1:-1].split('^')
+                chr_arr = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX'  # const
+                chars_to_symbols = {chr_arr[i]: symbols[i] or chr_arr[i] for i in range(len(chr_arr))}  # type: Dict[str, str]
+                p_exec = re_sub(r'\b(\w+)\b', lambda w: chars_to_symbols.get(w.group(1)) or '0', p_exec)
+                vals = dict()  # type: Dict[str, str]
+                for s in p_exec.split(';'):
+                    k, v = tuple(s.split('=', 1))
+                    val = 0
+                    for vs in v.split('^'):
+                        if vs.isnumeric():
+                            val ^= int(vs)
+                        else:
+                            assert vs in vals.keys()
+                            val ^= int(vals.get(vs))
+                    v = str(val)
+                    vals[k] = vals.get(v) or v
                 rows_raw = (re_sub(r'\)</script></font></td>.+', '',
                                    content_str.replace('<td colspan=1><font class=spy14>', '\n')
                                    .replace(r'<script type="text/javascript">document.write("<font class=spy2>:<\/font>"+', ':'))
@@ -58,7 +68,12 @@ def grab_proxies() -> None:
                 rows = [row for row in rows_raw if ip_re.match(row)]
                 for ri, row in enumerate(rows):
                     ip, expr = tuple(row.split(':', 1))
-                    port = ''.join(str(eval(e, locs)) for e in expr.split('+'))
+                    exprs = re_sub(r'[()]', '', re_sub(r'(\w+)', lambda w: vals.get(w.group(1)), expr)).split('+')
+                    for ei in range(len(exprs)):
+                        evs = exprs[ei].split('^')
+                        assert len(evs) == 2
+                        exprs[ei] = str(int(evs[0]) ^ int(evs[1]))
+                    port = ''.join(exprs)
                     rows[ri] = f'{ip}:{port}'
 
                 my_result += ''.join([format_proxy(s) for s in rows])
