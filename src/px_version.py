7d26d2ed917aba51a9e56ef45023590c91fa97fa
Trickerer01 <onlysuffering+1@gmail.com>
2025-10-14 18:04:02 +0700

    Setup .gitattributes, .gitignore, pyproject.toml, setup.cfg, hooks

--- src/px_version.py
+++ src/px_version.py
@@ -6,7 +6,7 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 #
 #
 
-APP_NAME = 'pxtest'
+APP_NAME = 'proxy_test'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '0'
 APP_REVISION = '99'
