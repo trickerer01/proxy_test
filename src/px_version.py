7a9f4ab09cf4be0d23a252e39a01d9de0fa76ed1
Trickerer01 <onlysuffering+1@gmail.com>
2025-10-18 18:35:49 +0700

    Broader ruff fixes at commit

--- src/px_version.py
+++ src/px_version.py
@@ -9,7 +9,7 @@ Author: trickerer (https://github.com/trickerer, https://github.com/trickerer01)
 APP_NAME = 'proxy_test'
 APP_VER_MAJOR = '1'
 APP_VER_SUB = '0'
-APP_REVISION = '101'
+APP_REVISION = '102'
 APP_VERSION = f'{APP_VER_MAJOR}.{APP_VER_SUB}.{APP_REVISION}'
 
 #
